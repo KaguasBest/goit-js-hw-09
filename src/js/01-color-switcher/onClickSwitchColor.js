6228b4d974a3442c38896216e76a5b6088a581cc
Yevhen <129059139+KaguasBest@users.noreply.github.com>
2023-10-21 16:32:17 +0300

    Update onClickSwitchColor.js

--- src/js/01-color-switcher/onClickSwitchColor.js
+++ src/js/01-color-switcher/onClickSwitchColor.js
@@ -2,11 +2,11 @@ import { changeColor } from './changeColor';
 import { refs } from './common';
 
 export function onClickStartSwitchColor() {
-  startChangeColor = setInterval(changeColor, 1000);
+  _startChangeColor = setInterval(changeColor, 1000);
 }
 
 export function onClickStopSwitchColor() {
-  clearInterval(startChangeColor);
+  clearInterval(_startChangeColor);
   refs.startBtn.disabled = false;
   refs.stopBtn.disabled = true;
 }
